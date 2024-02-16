a90c4e3ca3beab2b134b802811d637e24d61dd8a
Narendra <narendrakotika3576@gmail.com>
2024-02-16 10:12:00 +0530

    Add project files.

---
+++ CalcExample/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using CalcExample;
+
+Calc calc = new Calc();
+Console.WriteLine("Add Call");
+calc.Add(20, 35);
